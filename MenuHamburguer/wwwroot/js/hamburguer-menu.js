1dc1894d592c9d7419749951ad61a5044fae4dd4
Leomar <leomar.sartor@funcionalcorp.com.br>
2022-09-14 14:31:11 -0300

    Projeto Inicial

---
+++ MenuHamburguer/wwwroot/js/hamburguer-menu.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+
+    console.log("Carregando Menu.JS");
+
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+        isClosed = false;
+
+    trigger.click(function () {
+        hamburger_cross();
+    });
+
+    function hamburger_cross() {
+
+        console.log("CROSS");
+
+        if (isClosed == true) {
+
+            console.log("ABRE");
+
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {
+
+            console.log("FECHA");
+
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
+
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+        console.log("TROCOU");
+    });
+});
