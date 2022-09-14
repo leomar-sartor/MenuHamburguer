4b2aa703055d5d4c215b8881178b3244f1778a54
Leomar Sartor <leomar_sartor@unochapeco.edu.br>
2022-09-14 15:45:47 -0300

    Injeção de Dependência com serviços para buscar menus, arrumado Sub-Menu

---
+++ MenuHamburguer/MenuService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MenuHamburguer
+{
+    public class MenuService
+    {
+        public List<string> GetMenu()
+        {
+            return new List<string>() { "menu One", "Menu Two" };
+        }
+    }
+}
--- MenuHamburguer/Startup.cs
+++ MenuHamburguer/Startup.cs
@@ -23,6 +23,8 @@ namespace MenuHamburguer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddTransient<MenuService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
