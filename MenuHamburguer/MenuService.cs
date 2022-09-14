74c601084fe0ab56bf3a4aecf3f7ebd5a3c6f3c0
Leomar Sartor <leomar_sartor@unochapeco.edu.br>
2022-09-14 16:35:03 -0300

    Alteração do Menu para posteriormente adequar com as Roles/Claims

--- MenuHamburguer/MenuService.cs
+++ MenuHamburguer/MenuService.cs
@@ -4,9 +4,77 @@ namespace MenuHamburguer
 {
     public class MenuService
     {
-        public List<string> GetMenu()
+        public List<Menu> GetMenu()
         {
-            return new List<string>() { "menu One", "Menu Two" };
+            var menus = new List<Menu>();
+
+            var menuOne = new Menu()
+            {
+                Nome = "Dashboard",
+                Descricao = "Painel principal do sistema",
+                Area = "Administrador",
+                Controller = "dashboard",
+                Action = "Index"
+            };
+
+            var menuTwo = new Menu()
+            {
+                Nome = "Usuarios",
+                Descricao = "Painel principal do sistema",
+                Area = "",
+                Controller = "",
+                Action = "",
+                Itens = new List<Menu>()
+                {
+                    new Menu()
+                    {
+                        Nome = "Usuario",
+                        Descricao = "Cadastro de Usuarios",
+                        Area = "Usuario",
+                        Controller = "User",
+                        Action = "Index"
+                    },
+                    new Menu()
+                    {
+                        Nome = "Funções",
+                        Descricao = "Roles de Usuarios",
+                        Area = "Usuario",
+                        Controller = "Roles",
+                        Action = "Index"
+                    },
+                    new Menu()
+                    {
+                        Nome = "Declarações",
+                        Descricao = "Claims de Usuarios",
+                        Area = "Usuario",
+                        Controller = "Claims",
+                        Action = "Index"
+                    }
+                }
+            };
+
+            menus.Add(menuOne);
+            menus.Add(menuTwo);
+
+            return menus;
         }
     }
+
+    public class Menu
+    {
+        public Menu()
+        {
+            Itens = new List<Menu>();
+        }
+
+        public string Icone { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public string Area { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public List<Menu> Itens { get; set; }
+    }
 }
+
